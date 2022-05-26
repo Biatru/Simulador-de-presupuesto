b73f4fcfe9be55872b0196bd8524c92102974ccd
Bea <beabusaniche@gmail.com>
2022-05-26 20:04:06 -0300

    aplicando api fetch

--- js/main.js
+++ js/main.js
@@ -35,11 +35,11 @@ boton.onclick = ()=> {
       telefono: telefono.value,
       email: email.value,
    }
-  agregarDatos(local, cliente) /*esta creada en el archivo funciones.js*/
- 
+
+agregarDatos(local, cliente) /*esta creada en el archivo funciones.js*/
+
 }
 cargarCliente  (local, resultado) /*esta creada en el archivo funciones.js*/
-
 
 
 
@@ -171,8 +171,28 @@ generarPresupuesto.onclick = (estadia, habitacion) =>{
 
 const formulario = document.getElementById("formulario2");
  formulario.addEventListener ("submit", function(event) {
-    event.preventDefault();/*Cancela el comportamiento por defecto del navegador*/
+    event.preventDefault(); /*Cancela el comportamiento por defecto del navegador*/
     
  }
  )
- 
+/*
+PETICION POST CON LA API DE FETCH USANDO LA PAG. DE formsubmit que manda un mail cada vez que un usuario se registra con todos los datos*/
+
+boton.onclick = ()=> {
+    let cliente = {
+      id: Math.random(1,100),
+      nombre: nombre.value,
+      telefono: telefono.value,
+      email: email.value,
+   } 
+fetch ('https://formsubmit.co/ajax/beabusaniche@gmail.com', {
+   method: 'POST', /*Por defecto es GET*/
+   body: JSON.stringify(cliente), /*convierte el objeto para poder ser enviado*/
+   headers: {
+     'Content-type': 'application/json', /*cabezeras de informacion de lo que estamos enviando*/
+   },
+ })
+   .then((response) => response.json())
+   .then((json) => console.log(json))
+  
+}
